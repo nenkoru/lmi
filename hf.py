8adfa7b4952f37ae487ada24e05c1973a21aa4ce
Chernenko Ruslan <ractyfree@gmail.com>
2023-09-09 15:14:23 +0000

    Add exception handler

--- hf.py
+++ hf.py
@@ -185,6 +185,15 @@ async def startup_event():
     asyncio.create_task(batch_generating_loop())
 
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    print(exc.body)
+    logging.error(f"{request}: {exc_str}")
+    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", default="0.0.0.0")
